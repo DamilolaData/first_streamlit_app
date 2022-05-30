3cf9c1e3429f32a16d09a07988018ba30df11975
DamilolaData <86791114+DamilolaData@users.noreply.github.com>
2022-05-30 12:30:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,3 +54,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
 
+#Allow end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like information about?', 'jackfruit')
+streamlit.write('The user entered', add_my_fruit)
+
